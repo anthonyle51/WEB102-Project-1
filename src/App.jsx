345c4c4f4c8d87d3e4d943ed2c3de86586cc29fc
anthonyle51 <anthonyle20045@gmail.com>
2024-02-29 11:57:21 -0500

    level 3 modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Album from './components/Album';
 
 const App = () => {
 
   return (
     <div className="App">
-
-      
+      <h1>Favorite Albums</h1>
+      <div className="Albums">
+        <Album />
+      </div>
     </div>
   )
 }
---
+++ src/components/Album.jsx
@@ -0,0 +1,12 @@
+import './Album.css'
+
+const Album = (props) => {
+    return (
+        <div>
+            <img src='src\assets\flaws_and_all.webp' alt="Flaws and All" width="500" height="600"></img>
+            <h3>W</h3>
+        </div>
+    )
+}
+
+export default Album;
